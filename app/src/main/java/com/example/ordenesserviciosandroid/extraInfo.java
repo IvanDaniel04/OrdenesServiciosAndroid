b6183e65b31d3cd5d51f60b25d52fcac1b7bd4d8
Daniel Ivan Estrada Morando <125703648+ivandaniel04@users.noreply.github.com>
2023-04-30 20:25:09 -0600

    cambios de diseño extrainfoxml y otros requerimientos

--- app/src/main/java/com/example/ordenesserviciosandroid/extraInfo.java
+++ app/src/main/java/com/example/ordenesserviciosandroid/extraInfo.java
@@ -1,6 +1,8 @@
 package com.example.ordenesserviciosandroid;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,14 @@ public class extraInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.extrainfoxml);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinners);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.Estatus, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
         // Obtener el valor del extra "id" del Intent
         int id = getIntent().getIntExtra("id", 0);
         System.out.println(id);
